d2fe94d99a7eb156702418fac77ae6dfe4f34b52
bergloman <bergloman@gmail.com>
2024-04-18 09:24:41 +0200

    starting with more complex web app, added POST and state

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,32 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const port = +(process.env.PORT ?? "80");
 
+morgan.format("myformat", ":method :url :status len=:res[content-length] - time=:response-time ms");
+app.use(morgan("myformat", {
+    stream: {
+        write: (message: string) => {
+            console.log(message.trim());
+        }
+    }
+}));
+
+let counter = 0;
+
 app.get("/", (req, res) => {
     const name = req.query.name || "World";
-    res.send(`Hello ${name}!`);
+    res.send(`Hello ${name}! counter=${counter}`);
+});
+
+
+// curl -X POST http://localhost:PORT/inc
+app.post("/inc", (req, res) => {
+    counter++;
+    res.send(`counter=${counter}`);
 });
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
-
