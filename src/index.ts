c69f501be59f29b210cabe819064d36418af0e1e
bergloman <bergloman@gmail.com>
2024-04-23 13:51:22 +0200

    wip - added blob storage

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 import express from "express";
 import morgan from "morgan";
-import { ICache, RedisCache } from "./services";
+import { ICache, RedisCache, createBlobService } from "./services";
 
 ///////////////////////////////////////////////////////////////////////////
 
@@ -23,6 +23,9 @@ app.use(morgan("myformat", {
 
 let counter = 0;
 const cache: ICache = new RedisCache("counter");
+const blob_storage = createBlobService();
+const blob_container = "attachments";
+const blob_name = "my-blob";
 
 ///////////////////////////////////////////////////////////////////////////
 
@@ -42,6 +45,18 @@ app.post("/inc", async (req, res) => {
     const val = await cache.setStr(name, "" + counter, 10);
     res.send({ counter, name, val });
 });
+
+// curl -X POST http://localhost:PORT/blob-store -d '{}' -H "Content-Type: application/json"
+app.post("/blob-store", async (req, res) => {
+    await blob_storage.uploadBufferToBlob(blob_container, blob_name, {}, Buffer.from("Hello, World! " + (new Date().toISOString())));
+    res.send({});
+});
+// curl http://localhost:PORT/blob-store
+app.get("/blob-store", async (req, res) => {
+    const data = await blob_storage.downloadBufferFromBlob(blob_container, blob_name);
+    res.send("Data=" + data.toString());
+});
+
 
 ///////////////////////////////////////////////////////////////////////////
 
--- src/services.ts
+++ src/services.ts
@@ -1,4 +1,10 @@
 import Redis from "ioredis";
+import {
+    BlobServiceClient,
+    Metadata
+} from "@azure/storage-blob";
+
+const AZURE_BLOB_CONNECTION_STRING = process.env.AZURE_BLOB_CONNECTION_STRING || "";
 
 const REDIS_HOST = process.env.REDIS_HOST || "";
 const REDIS_PORT = parseInt(process.env.REDIS_PORT || "6380", 10);
@@ -86,3 +92,84 @@ export class RedisCache implements ICache {
         return await this.redis.exists(this.prefix_ + key) === 1;
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+export interface IBlobService {
+    uploadBufferToBlob(container_name: string, blob_name: string, metadata: Metadata, buffer: Buffer): Promise<void>;
+    downloadBufferFromBlob(container_name: string, blob_name: string): Promise<Buffer>;
+}
+
+export function canRunAzureBlob(): boolean {
+    return AZURE_BLOB_CONNECTION_STRING.length > 0;
+}
+
+/** Factory method for creating blob storage, according to env settings */
+export function createBlobService(): IBlobService {
+    if (canRunAzureBlob()) {
+        console.log("creating azure blob storage...");
+        return new AzureBlobService();
+    } else {
+        console.log("creating dummy blob storage...");
+        return new DummyBlobService();
+    }
+}
+
+/** Simple implementation of IBlobService that throws error when any method is called */
+export class DummyBlobService implements IBlobService {
+    map: Map<string, Buffer> = new Map<string, Buffer>();
+    public async uploadBufferToBlob(container_name: string, blob_name: string, metadata: Metadata, buffer: Buffer): Promise<void> { // eslint-disable-line @typescript-eslint/no-unused-vars
+        this.map.set(blob_name, buffer);
+    }
+    public async downloadBufferFromBlob(container_name: string, blob_name: string): Promise<Buffer> { // eslint-disable-line @typescript-eslint/no-unused-vars
+        return this.map.get(blob_name) || Buffer.from("");
+    }
+}
+
+/** Blob service that stores data in Azure BLOB storage */
+export class AzureBlobService implements IBlobService {
+
+    public async uploadBufferToBlob(container_name: string, blob_name: string, metadata: Metadata, buffer: Buffer): Promise<void> {
+        console.log(`Uploading ${blob_name} to ${container_name}`);
+        const blob_service_client = BlobServiceClient.fromConnectionString(AZURE_BLOB_CONNECTION_STRING);
+        const container_client = blob_service_client.getContainerClient(container_name);
+        await container_client.createIfNotExists();
+        // upload blob
+        const blockBlobClient = container_client.getBlockBlobClient(blob_name);
+        await blockBlobClient.upload(buffer, buffer.length);
+        // add metadata
+        await blockBlobClient.setMetadata(metadata);
+        console.log(`Upload of ${blob_name} to ${container_name} complete`);
+    }
+
+    public async downloadBufferFromBlob(container_name: string, blob_name: string): Promise<Buffer> {
+        const blob_service_client = BlobServiceClient.fromConnectionString(AZURE_BLOB_CONNECTION_STRING);
+        const container_client = blob_service_client.getContainerClient(container_name);
+        const block_blob_client = container_client.getBlockBlobClient(blob_name);
+        // get stream
+        const res = await block_blob_client.download();
+        if (!res.readableStreamBody) {
+            throw new Error("Downloaded stream is undefined");
+        }
+        // download to buffer
+        const buffer = await streamToBuffer(res.readableStreamBody);
+        console.log(`Download of ${blob_name} from ${container_name} complete`);
+        return buffer;
+    }
+}
+
+
+// Helper function to convert a readable stream to a Buffer
+async function streamToBuffer(stream: NodeJS.ReadableStream): Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+        const chunks: Buffer[] = [];
+        stream.on("data", (data) => {
+            chunks.push(data instanceof Buffer ? data : Buffer.from(data));
+        });
+        stream.on("end", () => {
+            resolve(Buffer.concat(chunks));
+        });
+        stream.on("error", reject);
+    });
+}
+
